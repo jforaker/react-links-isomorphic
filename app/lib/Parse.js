cca1482d2e3451c0d0b8f196a186dc6f2b8fdf3b
Jake Foraker <jake.foraker@byjakt.com>
2015-09-19 20:12:52 -0400

    adding parse as module, dotenv

--- app/lib/Parse.js
+++ app/lib/Parse.js
@@ -2,6 +2,6 @@
 var Parse = require('node-parse-api').Parse;
 
 module.exports = new Parse({
-    app_id: '4aM4ir2NIena1vdc2OZgMnH9K3vIg9Ej6LON2j3j',
-    master_key: 'w6YOQOiWLrszG2CWKR8OmBhEXwgZia4dKoRzb88E'
+    app_id: process.env.PARSE_APP_ID,
+    master_key: process.env.PARSE_MASTER_KEY
 });
