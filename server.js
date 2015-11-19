1e913c83e84b1a387940395d4f98356ff2d724fa
Jake Foraker <jake.foraker@byjakt.com>
2015-11-18 23:31:15 -0500

    adding comments

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').load();
-}
-var express = require('express'),
-    path = require('path'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    _ = require('lodash'),
-    http = require('http'),
-    inspect = require('eyes').inspector(),
-    bodyParser = require('body-parser'),
-    socketio = require('socket.io')
-    ;
+
+/************************************************
+ JAKT Slack links isomorphic react app
+ created by jforaker https://github.com/jforaker
+ https://github.com/jforaker/react-links-isomorphic
+ ***********************************************/
+
+require('dotenv').load();
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 3000;
+var _ = require('lodash');
+var http = require('http');
+var inspect = require('eyes').inspector();
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
 var React = require('react/addons');
 var SlackStrategy = require('passport-slack').Strategy;
 var mongoose = require('mongoose');
@@ -36,20 +40,18 @@ app.set('view engine', 'ejs');
 app.set('port', port);
 app.use(cookieParser());
 app.use(bodyParser());
-app.use(session({secret: 'my_precious'}));
+app.use(session({secret: 'Methionylglutaminylarginyltyrosylglutamyl'}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(express.static(__dirname + '/public'));
 
-// seralize and deseralize
+// seralize and deseralize user
 passport.serializeUser(function (user, done) {
-    console.log('serializeUser: ' + user._id);
     done(null, user._id);
 });
 passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
-        console.log(user);
         if (!err) done(null, user);
         else done(err, null);
     })
@@ -108,7 +110,7 @@ passport.use(new SlackStrategy({
 
             } else {
                 // user already exists and is logged in, we have to link accounts
-                inspect('else called');
+                inspect('else called on !req.user');
                 var user = req.user; // pull the user out of the session
                 user.slack.SlackId = profile.id;
                 user.slack.token = accessToken;
@@ -126,7 +128,7 @@ passport.use(new SlackStrategy({
 
 app.get('/', isLoggedIn, function (req, res) {
     var currentUser = req.user;
-    inspect(currentUser.slack.name, 'currentUser name');
+    inspect(currentUser.slack.name, 'currentUser name / redirect to /main');
     if (currentUser) {
         res.redirect('/main');
     } else {
@@ -161,49 +163,35 @@ app.get('/login', function (req, res) {
     }
 });
 
-//Slack
+/***********************************************
+    Passport slack
+ ***********************************************/
 
 app.get('/auth/slack', passport.authenticate('slack'));
 
 app.get('/auth/slack/callback',
     passport.authenticate('slack', {passReqToCallback: true, failureRedirect: '/'}), function (req, res) {
-        console.log('req.user /auth/slack/callback ', req.user);
         res.redirect('/main');
     });
 
+/***********************************************
+ ***********************************************/
 
-/////
 app.get('/main', isLoggedIn, function (req, res) {
-
-    inspect(req.user.slack, 'req.user get /main');
-
-    User.findById(req.session.passport.user, function (err, user) {
-        if (err) {
-            console.log(err);
-        } else {
-            inspect(user.slack, 'user fuck ya');
-            //res.render('account', {user: user});
-
-            //todo -- this works
-        }
-    });
 
     var links = [];
     var channels = [];
     var users = [];
 
     slacker.getSlackUsers().then(function (userdata) {
-
         return _.each(userdata, function (user) {
-            users.push(user);
+            users.push(user);  //create users array for frontend matching profile pics etc.
         });
 
     }).then(function () {
 
         slacker.getSlackChannels().then(function (data) {
-
             _.each(data.channels, function (channel) {
-
                 if (!channel.is_archived) {
                     channels.push({
                         name: channel.name,
@@ -211,20 +199,15 @@ app.get('/main', isLoggedIn, function (req, res) {
                     });
                 }
             });
-
-            return channels;
+            return channels; //get slack channels to store in dropdown filter
 
         }).then(function (channelz) {
 
             parseHelper.getLinks().then(function (linkz) {
 
                 _.each(linkz, function (item, i) {
-                    if (!_.has(item, 'upvotes')) {
-                        _.extend(item, {
-                            upvotes: 0
-                        });
-                    }
-                    links.push(item);
+                    if (!_.has(item, 'upvotes')) { _.extend(item, { upvotes: 0 }); } //if no upvotes, add 0 value for upvotes
+                    links.push(item); //return links from /link slash command on parse and extend them with 0 vote ^
                 });
 
                 // React.renderToString takes your component and generates the markup
@@ -253,10 +236,11 @@ app.get('/main', isLoggedIn, function (req, res) {
 
 app.post('/saved', function (req, res) {
 
-    /*
-     https://www.parse.com/apps/slack-bot/cloud_code/webhook#
-     https://jakt-slack-links.herokuapp.com/saved
-     */
+    /****
+         https://www.parse.com/apps/slack-bot/cloud_code/webhook#
+                ^^webhook called when a link is saved - show it in the Notifications component on the frontend
+         https://jakt-slack-links.herokuapp.com/saved
+     ****/
 
     inspect(req.body, '/link req.body');
 
@@ -273,12 +257,9 @@ app.get('server').listen(port, function () {
 });
 
 function isLoggedIn(req, res, next) {
+    //express middleware to detect session login state
     inspect(req.isAuthenticated(), 'req.isAuthenticated()');
-
-    if (req.isAuthenticated()) {
-        return next();
-    }
-
+    if (req.isAuthenticated()) return next(); //if authenticated user, do next action, otherwise redirect to login page
     res.redirect('/login');
 }
 
